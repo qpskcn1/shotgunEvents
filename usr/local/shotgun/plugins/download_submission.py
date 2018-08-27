fa5f71b4670159e6f0d3630a8e154ce1844242c8
qpskcn1 <yizheng0831@gmail.com>
2018-08-27 11:14:56 -0400

    Downlaod files to shotgun path

--- usr/local/shotgun/plugins/download_submission.py
+++ usr/local/shotgun/plugins/download_submission.py
@@ -1,15 +1,19 @@
-# Copyright 2018 Autodesk, Inc.  All rights reserved.
-#
-# Use of this software is subject to the terms of the Autodesk license agreement
-# provided at the time of installation or download, or which otherwise accompanies
-# this software in either electronic or hard copy form.
-#
+"""
+Plugin to download all submissions of remote artists to local storage server
 
-# See docs folder for detailed usage info.
+Author: Yi Zheng
+Copyright 2018 OHIO FILM GROUP All rights reserved.
+"""
+
 import os
-import sys
 import sgtk
 import shotgun_api3
+
+# Grab authentication env vars for this plugin. Install these into the env
+# if they don't already exist.
+SERVER = os.environ["SG_SERVER"]
+SCRIPT_NAME = os.environ["SGDAEMON_DOWNLOAD_NAME"]
+SCRIPT_KEY = os.environ["SGDAEMON_DOWNLOAD_KEY"]
 
 
 def registerCallbacks(reg):
@@ -19,14 +23,8 @@ def registerCallbacks(reg):
     :param reg: A Registrar instance provided by the event loop handler.
     """
 
-    # Grab authentication env vars for this plugin. Install these into the env
-    # if they don't already exist.
-    server = "https://ofg.shotgunstudio.com"
-    script_name = "download_submission"
-    script_key = "Fhhupdtir2nspoos&guxuraiq"
-
     # Grab an sg connection for the validator.
-    sg = shotgun_api3.Shotgun(server, script_name=script_name, api_key=script_key)
+    sg = shotgun_api3.Shotgun(SERVER, script_name=SCRIPT_NAME, api_key=SCRIPT_KEY)
 
     # Bail if our validator fails.
     if not is_valid(sg, reg.logger):
@@ -38,12 +36,12 @@ def registerCallbacks(reg):
     }
     # Temp solution for remote artist
     args = {
-        "applied_users": ["Allie Vanaman", "Yi Zheng"],
+        "applied_group": "Remote Artists",
     }
     # Register our callback with the Shotgun_Version_Change event
     reg.registerCallback(
-        script_name,
-        script_key,
+        SCRIPT_NAME,
+        SCRIPT_KEY,
         download_submission,
         eventFilter,
         args,
@@ -87,9 +85,14 @@ def download_submission(sg, logger, event, args):
     # Bail if we don't have the info we need.
     if not event_project:
         return
-    user_name = event.get("user").get("name")
-    if user_name not in args["applied_users"]:
-        logger.debug("{} is not a remote artist.".format(user_name))
+    # Bail if artist is not in applied group
+    user_id = event.get("user").get("id")
+    groups = sg.find_one("HumanUser", [["id", "is", user_id]], ["groups"])["groups"]
+    in_group = False
+    for group in groups:
+        if group["name"] == args["applied_group"]:
+            in_group = True
+    if not in_group:
         return
     # logger.info("%s" % str(event))
     event_meta = event.get("meta")
@@ -101,9 +104,66 @@ def download_submission(sg, logger, event, args):
         [["id", "is", entity_id]],
         ["code", "entity", "sg_task", "user", "sg_uploaded_movie"]
     )
+    # if entity or sg_task or sg_uploaded movie field is missing
+    # do not download anything
+    if not version["entity"] or \
+       not version["sg_task"] or \
+       not version["sg_uploaded_movie"]:
+        logger.warning("File missing required field, cancel download!")
+        return
     logger.debug("version {}".format(version))
+    # get path for this version
+    path = get_file_path(sg, event_project, version)
+    sg.download_attachment(version["sg_uploaded_movie"], file_path=path)
+    logger.info("File downloaded to {}".format(path))
 
 
-def get_tk(project):
-    tk = sgtk.sgtk_from_path()
-
+def get_file_path(sg, project, version):
+    # Use the authenticator to create a user object. This object
+    # identifies a Shotgun user or script and also wraps around
+    # a Shotgun API instance which is associated with that user
+    sa = sgtk.authentication.ShotgunAuthenticator()
+    script_user = sa.create_script_user(api_script=SCRIPT_NAME,
+                                        api_key=SCRIPT_KEY,
+                                        host=SERVER)
+    sgtk.set_authenticated_user(script_user)
+    # get toolkit manager
+    mgr = sgtk.bootstrap.ToolkitManager(sg_user=script_user)
+    mgr.plugin_id = "basic.*"
+    project_id = project.get("id")
+    engine = mgr.bootstrap_engine("tk-shell",
+                                  entity={"type": "Project", "id": project_id})
+    tk = engine.sgtk
+    sg_asset_type = sg.find_one(
+        "Asset",
+        [["id", "is", version["entity"]["id"]]],
+        ["sg_asset_type"]
+    )["sg_asset_type"]
+    step_id = sg.find_one(
+        "Task",
+        [["id", "is", version["sg_task"]["id"]]],
+        ["step"]
+    )["step"]["id"]
+    step = sg.find_one(
+        "Step",
+        [["id", "is", step_id]],
+        ["short_name"]
+    )["short_name"]
+    version_name = version["code"].split(".")
+    name = version_name[0].split("_")[-1]
+    version_number = version_name[1]
+    if version_number.startswith("v"):
+        version_number = version_number[1:]
+    work_fields = {
+        "Asset": version["entity"]["name"],
+        "sg_asset_type": sg_asset_type,
+        "Step": step,
+        "name": name,
+        "version": int(version_number),
+    }
+    ps_template = tk.templates["photoshop_asset_work"]
+    path = ps_template.apply_fields(work_fields)
+    engine.ensure_folder_exists(os.path.dirname(path))
+    # destroy
+    engine.destroy()
+    return path
